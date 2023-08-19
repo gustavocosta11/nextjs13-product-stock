b66ae5025f8e5afe51e2067857beff6ce90f35b6
Gustavo Costa <gustavo.lcostaa@gmail.com>
2023-08-18 19:14:42 -0700

    stock project demo done

---
+++ actions/serverActions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { Product } from "@/typings";
+import { revalidateTag } from "next/cache";
+
+export const addProductToDatabase = async (e: FormData) => {
+  const product = e.get("product")?.toString();
+  const price = e.get("price")?.toString();
+
+  if (!product || !price) return;
+
+  const newProduct: Product = {
+    product,
+    price,
+  };
+
+  await fetch("https://64dd6032e64a8525a0f7d767.mockapi.io/products", {
+    method: "POST",
+    body: JSON.stringify(newProduct),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  revalidateTag("products");
+};
---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import { addProductToDatabase } from "@/actions/serverActions";
+import AddProductButton from "@/components/AddProductButton";
+import { Product } from "@/typings";
+
+export default async function Home() {
+  const res = await fetch(
+    "https://64dd6032e64a8525a0f7d767.mockapi.io/products",
+    {
+      cache: "no-cache",
+      next: {
+        tags: ["products"],
+      },
+    }
+  );
+
+  const products: Product[] = await res.json();
+
+  return (
+    <main className="">
+      <h1 className="text-3xl font-bold text-center">Products Warehouse</h1>
+
+      <AddProductButton />
+
+      <form
+        action={addProductToDatabase}
+        className="flex flex-col gap-5 max-w-xl mx-auto p-5"
+      >
+        <input
+          name="product"
+          placeholder="Enter Product Name..."
+          className="border border-gray-300 p-2 rounded-md"
+        />
+        <input
+          name="price"
+          placeholder="Enter Product Price..."
+          className="border border-gray-300 p-2 rounded-md"
+        />
+        <button className="border bg-blue-500 text-white p-2 rounded-md">
+          Add Product
+        </button>
+      </form>
+
+      <h2 className="font-bold p-5">List of Products</h2>
+      <div className="flex flex-wrap gap-5">
+        {products.map((product) => (
+          <div key={product.id} className="p-5 shadow">
+            <p>{product.product}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
